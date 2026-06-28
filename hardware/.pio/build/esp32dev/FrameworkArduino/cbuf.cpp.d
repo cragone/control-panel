c2fa571a4c40bca9a5bb200036e029f3c21f6889
cragone <ragonecharlie@gmail.com>
2026-06-27 21:49:10 -0400

    address fix

--- hardware/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/cragone.HD-CRAGONE/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/cragone.HD-CRAGONE/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
